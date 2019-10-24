5187a97fde31400151773b898bbd6df1c25355f6
Bridger Middleton <bmiddy3@gmail.com>
2019-10-24 17:12:40 -0600

    first commit

---
+++ apps/football_app/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.loginpage),
+    url(r'^registerpage$', views.registerpage),
+    url(r'^login$', views.login),
+    url(r'^register$', views.register),
+    url(r'^logout$', views.logout),
+    url(r'^draftpage$', views.draftpage),
+    url(r'^draftplayer$', views.draftplayer),
+    url(r'^reset$', views.reset),
+    url(r'^teamHome$', views.teamHome),
+    url(r'^individualplayerpage/(?P<playerid>\d+)/(?P<week>\d+)$', views.individualplayerpage),
+    url(r'^week/(?P<week>\d+)/(?P<playerid>\d+)$', views.eachweekstats),
+    url(r'^home$', views.home),
+    url(r'^home/(?P<val>[1-8]{1})$', views.homeWeek),
+    url(r'^matchup/(?P<val>[1-8]{1})/(?P<match>[0-4]{1})$', views.matchup)
+]
